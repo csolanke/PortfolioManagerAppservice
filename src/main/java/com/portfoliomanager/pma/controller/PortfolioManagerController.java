026b08a14db6b1acdeba143db3f35aab8ed46adc
Chandrakant <chandrakant@Chandrakants-MacBook-Pro.local>
2021-12-25 22:37:02 -0500

    Made changes to use in memory database

--- src/main/java/com/portfoliomanager/pma/controller/PortfolioManagerController.java
+++ src/main/java/com/portfoliomanager/pma/controller/PortfolioManagerController.java
@@ -3,6 +3,7 @@ package com.portfoliomanager.pma.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import com.portfoliomanager.pma.dto.StockDTO;
 import com.portfoliomanager.pma.mapper.StockMapper;
 import com.portfoliomanager.pma.service.StockService;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class PortfolioManagerController {
